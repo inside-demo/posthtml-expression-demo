12095e6d7468158c942ddf766064f5f052744e86
Ivan Demidov <scrum@list.ru>
2017-12-13 16:54:53 +0300

    demo

---
+++ index.js
@@ -0,0 +1,23 @@
+import {readFileSync, writeFileSync} from 'fs';
+import posthtml from 'posthtml';
+
+const html = readFileSync('templates/index.html', 'utf8');
+const plugins = [
+	require('posthtml-extend')({
+    	encoding: 'utf8',
+    	root: 'templates'
+	}),
+	// require('posthtml-expressions')({ 
+	// 	locals: { 
+	// 		className: 'intro', 
+	// 		name: 'Marlo',
+	// 		foo: 'foo'
+	// 	}
+	// }),
+	require('posthtml-beautify')()
+]
+posthtml(plugins)
+    .process(html)
+    .then(result => {
+        writeFileSync('dist/output.html', result.html);
+    });
